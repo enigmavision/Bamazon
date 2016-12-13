38103cf5025900e869cd02da59292d5520e89e5e
enigma <deepaul.bharj@gmail.com>
2016-12-13 17:08:53 -0600

    renamed database variable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,42 +1,86 @@
-var mysql = require("mysql");
-var prompt = require('prompt');
+//Dependencies
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
 
+//Connection
 var connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
     port: 3306,
-
-    // Database Username
-    user: "root",
-
-    //Database Password
-    password: "",
+    user: "root", //Your username
+    password: "", //Your password
     database: "Bamazon"
 });
 
-connection.connect(function(err, res) {
-    if (err) throw err;
+//Functions
+function displayAll() {
+    //show all ids, names, and products from database.
+    connection.query('SELECT * FROM StoreProducts', function(error, response) {
+        if (error) { console.log(error) };
+        //New instance of our constructor
+        var theDisplayTable = new Table({
+            //declare the value categories
+            head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity'],
+            //set widths to scale
+            colWidths: [10, 30, 18, 10, 14]
+        });
+        //for each row of the loop
+        for (i = 0; i < response.length; i++) {
+            //push data to table
+            theDisplayTable.push(
+                [response[i].ItemID, response[i].ProductName, response[i].DepartmentName, response[i].Price, response[i].StockQuantity]
+            );
+        }
+        //log the completed table to console
+        console.log(theDisplayTable.toString());
+        inquireForPurchase();
+    });
 
-    console.log("connected as id " + connection.threadId);
-});
 
-connection.query('SELECT * FROM products', function(error, res) {
-    if (error) throw eror;
-    for (var i = 0; i < res.length; i++) {
-        console.log(res[i].product_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-    }
+}; //end displayAll
+function inquireForPurchase() {
+    //get item ID and desired quantity from user. Pass to purchase from Database
+    inquirer.prompt([
 
-});
+        {
+            name: "ID",
+            type: "input",
+            message: "What is the item number of the item you wish to purchase?"
+        }, {
+            name: 'Quantity',
+            type: 'input',
+            message: "How many would you like to buy?"
+        },
 
-  prompt.start();
+    ]).then(function(answers) {
+        //set captured input as variables, pass variables as parameters.
+        var quantityDesired = answers.Quantity;
+        var IDDesired = answers.ID;
+        purchaseFromDatabase(IDDesired, quantityDesired);
+    });
 
-  prompt.get(['product_id', 'product_stock'], function (err, result) {
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('  prod: ' + result.product_id);
-    console.log('  product_stock: ' + result.product_stock);
-  });
+}; //end inquireForPurchase
 
-  function onErr(err) {
-    console.log(err);
-    return 1;
-  }
+function purchaseFromDatabase(ID, quantityNeeded) {
+    //check quantity of desired purchase. Minus quantity of the itemID from database if possible. Else inform user "Quantity desired not in stock"
+    connection.query('SELECT * FROM StoreProducts WHERE ItemID = ' + ID, function(error, response) {
+        if (error) { console.log(error) };
+
+        //if in stock
+        if (quantityNeeded <= response[0].StockQuantity) {
+            //calculate cost
+            var totalCost = response[0].Price * quantityNeeded;
+            //inform user
+            console.log("We have what you need! I'll have your order right out!");
+            console.log("Your total cost for " + quantityNeeded + " " + response[0].ProductName + " is " + totalCost + ". Thank you for your Business!");
+            //update database, minus purchased quantity
+            connection.query('UPDATE StoreProducts SET StockQuantity = StockQuantity - ' + quantityNeeded + ' WHERE ItemID = ' + ID);
+        } else {
+            console.log("Our apologies. We don't have enough " + response[0].ProductName + " to fulfill your order.");
+        };
+        displayAll();//recursive shopping is best shopping! Shop till you drop!
+    });
+
+}; //end purchaseFromDatabase
+
+displayAll();
